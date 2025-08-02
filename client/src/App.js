a3a7af0260681f0d428b7321b88b891f90172cd7
KOOH <koohwebdev@gmail.com>
2025-08-03 00:38:51 +0800

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import "./App.css";
+import UserForm from "./components/UserForm.jsx";
+import UserList from "./components/UserList.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm />
+      <UserList>
+
+      </UserList>
+      </div>
+  );
+}
+
+export default App;
